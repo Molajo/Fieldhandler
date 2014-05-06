4034ca4fd2585941f1e63d8a6ae42b9eee5331f3
AmyStephen <AmyStephen@gmail.com>
2014-05-06 18:15:53 -0500

    Uppercase

---
+++ Source/Constraint/AbstractString.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * AbstractString Constraint
+ *
+ * @package    Molajo
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+namespace Molajo\Fieldhandler\Constraint;
+
+use CommonApi\Model\ConstraintInterface;
+
+/**
+ * AbstractString Constraint
+ *
+ * @package    Molajo
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @since      1.0.0
+ */
+abstract class AbstractString extends AbstractConstraint implements ConstraintInterface
+{
+    /**
+     * String Function
+     *
+     * @var    string
+     * @since  1.0.0
+     */
+    protected $string_function;
+
+    /**
+     * Validate
+     *
+     * @return  boolean
+     * @since   1.0.0
+     */
+    public function validate()
+    {
+        if ($this->field_value === null) {
+            return true;
+        }
+
+        $temp = $this->doStringFunction();
+
+        if ($temp === $this->field_value) {
+            return true;
+        }
+
+        $this->setValidateMessage(2000);
+
+        return false;
+    }
+
+    /**
+     * Sanitize
+     *
+     * @return  null|string
+     * @since   1.0.0
+     */
+    public function sanitize()
+    {
+        if ($this->field_value === null) {
+            return $this->field_value;
+        }
+
+        $temp = $this->doStringFunction();
+
+        if ($this->field_value === $temp) {
+        } else {
+            $this->field_value = null;
+        }
+
+        return $this->field_value;
+    }
+
+    /**
+     * Format
+     *
+     * @return  string|null
+     * @since   1.0.0
+     */
+    public function format()
+    {
+        return $this->doStringFunction();
+    }
+
+    /**
+     * Format
+     *
+     * @return  string|null
+     * @since   1.0.0
+     */
+    public function doStringFunction()
+    {
+        if ($this->string_function === 'trim') {
+            return trim($this->field_value);
+        } elseif ($this->string_function === 'lower') {
+            return strtolower($this->field_value);
+        } elseif ($this->string_function === 'upper') {
+            return strtoupper($this->field_value);
+        }
+    }
+}
