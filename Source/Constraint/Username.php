004e269a8155556199c110a89e91389c26f96721
AmyStephen <AmyStephen@gmail.com>
2014-05-18 11:28:23 -0500

    Username

---
+++ Source/Constraint/Username.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Username Constraint
+ *
+ * @package    Molajo
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+namespace Molajo\Fieldhandler\Constraint;
+
+use CommonApi\Model\ConstraintInterface;
+
+/**
+ * Username Constraint
+ *
+ * @link       http://php.net/manual/en/function.is-string.php
+ * @package    Molajo
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @since      1.0.0
+ */
+class Username extends String implements ConstraintInterface
+{
+
+}
