71b0d29c850d543d3291ba0b6f26347de31c5338
AmyStephen <AmyStephen@gmail.com>
2014-05-18 12:09:08 -0500

    Integer

---
+++ Source/Constraint/Userid.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Userid Constraint
+ *
+ * @package    Molajo
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+namespace Molajo\Fieldhandler\Constraint;
+
+use CommonApi\Model\ConstraintInterface;
+
+/**
+ * Userid Constraint
+ *
+ * Includes only digits, plus and minus sign.
+ *
+ * #### Validate
+ *
+ * Verifies value against constraint, returning a TRUE or FALSE result and error messages
+ *
+ * This example returns true.
+ *
+ * ```php
+ * $response = $request->validate('userid_field', 100, 'Userid');
+ *
+ * if ($response->getValidateResponse() === true) {
+ *     // all is well
+ * } else {
+ *     foreach ($response->getValidateMessages as $code => $message) {
+ *         echo $code . ': ' . $message . '/n';
+ *     }
+ * }
+ *
+ * ```
+ *
+ * #### Sanitize
+ *
+ * Removes characters not conforming to the definition of the constraint. In this example,
+ *  `$field_value` will result in NULL.
+ *
+ * ```php
+ * $response = $request->sanitize('userid_field', 'AmyStephen@gmail.com', 'Userid');
+ *
+ * if ($response->getChangeIndicator() === true) {
+ *     $field_value = $response->getFieldValue();
+ * }
+ *
+ * ```
+ *
+ * #### Format
+ *
+ * Not implemented, simply returns the value sent in.
+ *
+ * @api
+ * @link       http://us1.php.net/manual/en/filter.filters.sanitize.php
+ * @link       http://us1.php.net/manual/en/filter.filters.validate.php
+ * @package    Molajo
+ * @copyright  2014 Amy Stephen. All rights reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @since      1.0.0
+ */
+class Userid extends Integer implements ConstraintInterface
+{
+
+}
